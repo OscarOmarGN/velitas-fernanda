44b6ab1595348feb310467102d1e2343c9b65fb5
Oscar Garcia <49881499+OscarOmarGN@users.noreply.github.com>
2025-08-05 16:20:04 -0600

    update

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
-  
+const velitaNombre = document.querySelectorAll('.velita');
+
+const hamburguesa = document.querySelector('#hamburguesa');
+const enlaces = document.querySelector('#nav-links');
+// const nombreVelita = document.querySelectorAll('.nombre-velita');
+
+
+hamburguesa.addEventListener('click', () => {
+  enlaces.classList.toggle('hidden');
+});
+
+
+
+
+
+
+
+
